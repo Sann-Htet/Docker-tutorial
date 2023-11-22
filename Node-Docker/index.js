3037fd6a1abdcb60fc2452c69672d4f633160937
Sann-Htet <sannhtet899@gmail.com>
2023-11-23 04:27:56 +0630

    working with node app and mongo db services

--- Node-Docker/index.js
+++ Node-Docker/index.js
@@ -1,6 +1,13 @@
 const express = require("express")
+const mongoose = require("mongoose");
+
 
 const app = express();
+
+mongoose
+    .connect("mongodb://sannhtet:mypassword@mongo:27017/?authSource=admin")
+    .then(() => console.log("successfully connected to DB"))
+    .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
     res.send("<h2>Hi There</h2>")
